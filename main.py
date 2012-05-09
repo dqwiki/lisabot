ce6d5cf7d825c3a1ea735e02415eaeca5e92118b
dqwiki <deltaquadwiki@gmail.com>
2012-05-08 20:15:50 -0400

    kill the extra bold red text...or attempt to

--- main.py
+++ main.py
@@ -284,7 +284,7 @@ def tellFreenode(msg):
                                 if bline[0].lower() in page.lower() and bline[1] == line[1]:
                                         return
                         if not line[1] in alreadyprint:
-                                msg = msg.replace(":", ": \x0304(Matched user: " + line[0] + ")\x0301", 1)
+                                msg = msg.replace(":", "\x0F: \x0304(Matched user: " + line[0] + ")\x0301", 1)
                                 say(msg, line[1])
                         time.sleep(0.5)
                         alreadyprint = alreadyprint + "," + line[1]
@@ -295,7 +295,7 @@ def tellFreenode(msg):
                                 if bline[0].lower() in page.lower() and bline[1] == line[1]:
                                         return
                         if not line[1] in alreadyprint:
-                                msg = msg.replace(":", ": \x0304(Matched page: [[" + line[0] + "]])\x0301", 1)
+                                msg = msg.replace(":", "\x0F: \x0304(Matched page: [[" + line[0] + "]])\x0301", 1)
                                 say(msg, line[1])
                         time.sleep(0.5)
                         alreadyprint = alreadyprint + "," + line[1]
@@ -305,7 +305,7 @@ def tellFreenode(msg):
                                 if bline[0].lower() in summary.lower() and bline[1] == line[1]:
                                         return
                         if not line[1] in alreadyprint:
-                                msg = msg.replace(":", ": \x0304(Matched edit summary: " + line[0] + ")\x0301", 1)
+                                msg = msg.replace(":", "\x0F: \x0304(Matched edit summary: " + line[0] + ")\x0301", 1)
                                 say(msg, line[1])
                         time.sleep(0.5)
                         alreadyprint = alreadyprint + "," + line[1]
