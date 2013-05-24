62944bd5504f763d1099bd7c151cfeb498274bfd
DeltaQuad <deltaquadwiki@gmail.com>
2013-05-24 23:41:37 +0200

    update git ignore, remove .lower() statement

--- main.py
+++ main.py
@@ -255,7 +255,7 @@ def tellFreenode(msg,stalk,black):
                         try:
 		                for bline in black:
                                         if bline =="":continue
-                                        bline=bline.lower().split(",")
+                                        bline=bline.split(",")
 		                        if (bline[1] in page or bline[1] in user or bline[1] in summary) and bline[0] == channel:
                                                 print "!!! This ^^ ("+' '.join(bline[0:])+") bline RC entry is blacklisted. !!!"
 		                                return
@@ -277,7 +277,7 @@ def tellFreenode(msg,stalk,black):
 			try:
 		                for bline in black:
                                         if bline =="":continue
-                                        bline=bline.lower().split(",")
+                                        bline=bline.split(",")
 		                        if (bline[1] in page or bline[1] in user or bline[1] in summary) and bline[0] == channel:
                                                 print "!!! This ^^ ("+' '.join(bline[0:])+") bline RC entry is blacklisted. !!!"
 		                                return
@@ -298,7 +298,7 @@ def tellFreenode(msg,stalk,black):
 			try:
 		                for bline in black:
                                         if bline =="":continue
-                                        bline=bline.lower().split(",")
+                                        bline=bline.split(",")
 		                        if (bline[1] in page or bline[1] in user or bline[1] in summary) and bline[0] == channel:
                                                 print "!!! This ^^ ("+' '.join(bline[0:])+") bline RC entry is blacklisted. !!!"
 		                                return
